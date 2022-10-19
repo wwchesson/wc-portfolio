1a5632798fa46ac0c698fa7ea5df6de20bc5e130
Will <willchesson@gmail.com>
2022-10-19 15:14:23 -0400

    styling and images

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,57 @@
-import React, {useEffect, useState} from "react"
-import { Box, Typography, Card, CardMedia, CardContent, Grid, Container } from "@mui/material"
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Typography,
+  Card,
+  CardMedia,
+  CardContent,
+  Grid,
+  Container,
+  Link,
+} from "@mui/material";
 
 
 function App() {
-  const [websites, setWebsites] = useState([])
+  const [websites, setWebsites] = useState([]);
 
   useEffect(() => {
     fetch("/websites")
-    .then(r => r.json())
-    .then((data) => setWebsites(data))
-  }, [])
-
+      .then((r) => r.json())
+      .then((data) => setWebsites(data));
+  }, []);
 
   return (
     <div className="App">
-
-    <Box>
-      <Card id="welcome-card">
-        <CardContent >
-          <Typography >Welcome to Will's Portfolio</Typography>
-        </CardContent>
-      </Card>
-    </Box>
-
-    <Container maxWidth="md">
-    <Grid container spacing={4}>
-      {websites.map((website) => (
-        <Card>
-          <CardMedia>
-
-          </CardMedia>
+      <Box sx={{ marginBottom: "20px"}}>
+        <Card id="welcome-card">
           <CardContent>
-            
+            <Typography>Welcome to Will's Portfolio</Typography>
           </CardContent>
         </Card>
-      ))}
-    </Grid>
-    </Container>
+      </Box>
 
+      <Container maxWidth="md">
+        <Grid container spacing={4}>
+            {websites.map((website) => (
+              <Grid item xs={12} sm={6} md={4} >
+            <Card key={website.id} sx={{marginBottom: "30px"}}>
+              <CardMedia
+                component="img"
+                image={website.image}
+                height="250"
+              ></CardMedia>
+              <CardContent>
+                <a href={website.address} target="_blank">
+                  <Typography sx={{justifyContent: "center", display: "flex"}}>{website.name} App</Typography>
+                </a>
+              </CardContent>
+            </Card>
+            </Grid>
+          ))}
+          
+          
+        </Grid>
+      </Container>
     </div>
   );
 }
