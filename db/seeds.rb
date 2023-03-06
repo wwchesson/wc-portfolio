699be469d9bdc3af5ce2894309712d488dc694da
Will <willchesson@gmail.com>
2023-03-06 14:52:13 -0500

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ puts "Seeding y'all..."
 
 Website.create(name: "Georgia Wildlife", address: "https://radiant-boba-7d87fe.netlify.app/", image: "https://www1.nyc.gov/assets/wildlifenyc/images/content/pages/Stag_On_A_Hill.jpg", description: "Application for children to learn about naturally occurring wildlife and threatened animals in the state of Georgia.")
 
-Website.create(name: "impactABillion", address: "https://impactabillion.herokuapp.com/", image: "https://2.bp.blogspot.com/_JyEqAjSxEn0/TDzgTsjhxII/AAAAAAAAB7A/Wj4pv8y9XsU/s400/Edit_Water_Drops_0082.JPG", description: "Software solution for community building, service opportunities, and creative gatherings.")
+Website.create(name: "impactABillion", address: "https://impactabillion.onrender.com/", image: "https://2.bp.blogspot.com/_JyEqAjSxEn0/TDzgTsjhxII/AAAAAAAAB7A/Wj4pv8y9XsU/s400/Edit_Water_Drops_0082.JPG", description: "Software solution for community building, service opportunities, and creative gatherings.")
 
-Website.create(name: "Lake Claire Community for the Unhoused", image: "https://www.oregonlive.com/resizer/LimluW4E7MMaMW8pJlEFq0xa0co=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/OJ55YCOO35E3BEXXQJGKBH4EMQ.JPG", description: "Mock application for managers and clients of the unhoused community. Currently under construction.")
+Website.create(name: "Lake Claire Community for the Unhoused", address: "https://www.youtube.com/watch?v=OWB1Zl3se2c", image: "https://www.oregonlive.com/resizer/LimluW4E7MMaMW8pJlEFq0xa0co=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/OJ55YCOO35E3BEXXQJGKBH4EMQ.JPG", description: "Mock application for managers and clients of the unhoused community. Currently under construction.")
 
 puts "Finished seeding!"
