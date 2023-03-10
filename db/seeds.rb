f53b2a5cdf91f2ef5291d282c6be41eb8d3d24ed
Will <willchesson@gmail.com>
2023-03-10 13:54:24 -0500

    gawildlife

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 puts "Seeding y'all..."
 
-Website.create(name: "Georgia Wildlife", gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmZkZDA1OGMwMGRhZDZhNWE4YTVhNmQwMzI3NjNlMzVlNjBhZWQ0OSZjdD1n/PJ6K5SacrMfiHh8mm6/giphy.gif", address: "https://radiant-boba-7d87fe.netlify.app/", image: "https://www1.nyc.gov/assets/wildlifenyc/images/content/pages/Stag_On_A_Hill.jpg", description: "Application for children to learn about naturally occurring wildlife and threatened animals in the state of Georgia.")
+Website.create(name: "Georgia Wildlife", gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmZkZDA1OGMwMGRhZDZhNWE4YTVhNmQwMzI3NjNlMzVlNjBhZWQ0OSZjdD1n/PJ6K5SacrMfiHh8mm6/giphy.gif", address: "https://gawildlife.onrender.com/", image: "https://www1.nyc.gov/assets/wildlifenyc/images/content/pages/Stag_On_A_Hill.jpg", description: "Application for children to learn about naturally occurring wildlife and threatened animals in the state of Georgia.")
 
 Website.create(name: "impactABillion", gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzMxMWYxNDg0NmZhNDFjNzcyNjIwZWY5ZTFhYWQ0YjQ5ZjU3NmIwYiZjdD1n/JceLaiOnZUFM8rcdoT/giphy.gif", address: "https://impactabillion.onrender.com/", image: "https://2.bp.blogspot.com/_JyEqAjSxEn0/TDzgTsjhxII/AAAAAAAAB7A/Wj4pv8y9XsU/s400/Edit_Water_Drops_0082.JPG", description: "Software solution for community building, service opportunities, and creative gatherings.")
 
