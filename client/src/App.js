3a29a8887fb4e3a50d9275fcdefc9c12bb276ff4
Will <willchesson@gmail.com>
2023-03-10 08:47:41 -0500

    gif test

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,13 @@ import {
   CardContent,
   Grid,
   Container,
-  Link,
+  Button,
 } from "@mui/material";
-
+import gawildlife from "./gawildlife.gif";
 
 function App() {
   const [websites, setWebsites] = useState([]);
+  const [gif, setGif] = useState(false);
 
   useEffect(() => {
     fetch("/websites")
@@ -22,37 +23,62 @@ function App() {
 
   return (
     <div className="App">
-      <Box sx={{ marginBottom: "80px"}} >
+      <Box sx={{ marginBottom: "80px" }}>
         <Card id="welcome-card">
-          <CardContent sx={{margin: "auto", display: "flex"}}>
-            <Typography variant="h3"  style={{color: "white"}}><strong>Welcome to Will's Portfolio</strong></Typography>
+          <CardContent sx={{ margin: "auto", display: "flex" }}>
+            <Typography variant="h3" style={{ color: "white" }}>
+              <strong>Welcome to Will's Portfolio</strong>
+            </Typography>
           </CardContent>
         </Card>
       </Box>
 
+      {gif ? (
+        <Card sx={{display: "flex", height: "500px", width: "500px", margin: "auto", marginBottom: "50px"}}>
+          <CardMedia component="img" image={gawildlife}></CardMedia>
+        </Card>
+      ) : null}
+
       <Container maxWidth="md">
         <Grid container spacing={2}>
-            {websites.map((website) => (
-              <Grid item xs={12} sm={6} md={4} >
-            <Card key={website.id} sx={{marginBottom: "225px", marginLeft: "30px", height: "450px"}} style={{backgroundColor: "#adc4ad"}}>
-              <CardMedia
-                component="img"
-                image={website.image}
-                height="250"
-              ></CardMedia>
-              <CardContent >
-                <a href={website.address} target="_blank">
-                  <Typography sx={{justifyContent: "center", display: "flex"}}>{website.name}</Typography>
-                  
-                </a>
-                <br />
-                <Typography>{website.description}</Typography>
-              </CardContent>
-            </Card>
+          {websites.map((website) => (
+            <Grid item xs={12} sm={6} md={4}>
+              <Card
+                key={website.id}
+                sx={{
+                  marginBottom: "225px",
+                  marginLeft: "30px",
+                  height: "500px",
+                }}
+                style={{ backgroundColor: "#adc4ad" }}
+              >
+                <CardMedia
+                  component="img"
+                  image={website.image}
+                  height="250"
+                ></CardMedia>
+                <CardContent>
+                  {/* <a href={website.address} target="_blank"> */}
+                  <Typography
+                    sx={{ justifyContent: "center", display: "flex" }}
+                  >
+                    {website.name}
+                  </Typography>
+                  <Button onClick={() => setGif(!gif)}>
+                    <Typography
+                      sx={{ justifyContent: "center", display: "flex" }}
+                    >
+                      See Gif
+                    </Typography>
+                  </Button>
+
+                  {/* </a> */}
+                  <br />
+                  <Typography>{website.description}</Typography>
+                </CardContent>
+              </Card>
             </Grid>
           ))}
-          
-          
         </Grid>
       </Container>
     </div>
