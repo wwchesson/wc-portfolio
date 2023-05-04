d35b67abeaa558590d272b35b17d6677ff9a5af4
Will <willchesson@gmail.com>
2023-05-04 18:10:36 -0400

    navbar with menu

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,30 +1,70 @@
-import React from "react";
-import {Link} from "react-router-dom"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
   Box,
   Typography,
   Card,
   CardContent,
+  Menu,
+  MenuItem,
+  CardMedia,
+  Button,
 } from "@mui/material";
 import { GoMarkGithub } from "react-icons/go";
 import { BsLinkedin } from "react-icons/bs";
+import { HiMenu } from "react-icons/hi";
 
 function NavBar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
+
+  const handleOpen = (e) => {
+    setAnchorEl(e.currentTarget);
+    setOpen(true);
+  };
+
   return (
     <div className="navbar">
-      <Card
-        id="welcome-card"
-        sx={{ display: "flex", marginBottom: "1px", height: "90px" }}
+
+      <Card id="welcome-card" sx={{ display: "flex", height: "90px" }}>
+        <CardContent sx={{ marginTop: "1px", display: "flex", justifyItems: "center" }}>
+          <Button onClick={handleOpen}>
+            <Link style={{ fontSize: "3rem", color: "white" }}>
+              <HiMenu />
+            </Link>
+          </Button>
+
+          {/*menu logic*/}
+
+          <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+            <MenuItem component={Link} to="/aboutme">
+              About
+            </MenuItem>
+            <MenuItem component={Link} to="/">
+              Portfolio
+            </MenuItem>
+          </Menu>
+
+
+      <Typography
+        sx={{
+          backgroundColor: "black",
+          marginLeft: "1rem",
+          // position: "absolute",
+          marginTop: "0.5rem",
+          fontFamily: "fantasy",
+          // padding: "1rem"
+        }}
+        color="white"
+        variant="h5"
       >
-        <CardContent
-          sx={{ marginTop: "1px", marginLeft: "10px", display: "flex" }}
-        >
-          <Typography
-            variant="h3"
-            style={{ color: "white", fontFamily: "fantasy" }}
-          >
-            <strong>WC</strong>
-          </Typography>
+        <strong>Will's Portfolio</strong>
+      </Typography>
 
           <Box
             sx={{
@@ -34,26 +74,6 @@ function NavBar() {
               justifyContent: "space-evenly",
             }}
           >
-            <Typography variant="h6" sx={{ marginRight: "30px" }}>
-              <Link
-                to="/aboutme"
-                style={{ color: "white", fontFamily: "fantasy" }}
-              >
-                <strong>1. About</strong>
-              </Link>
-            </Typography>
-            <Typography variant="h6">
-              <Link
-                to="/"
-                style={{
-                  color: "white",
-                  fontFamily: "fantasy",
-                  marginRight: "15px",
-                }}
-              >
-                <strong>2. Portfolio</strong>
-              </Link>
-            </Typography>
             <Link
               to="https://github.com/wwchesson"
               target="_blank"
@@ -64,7 +84,12 @@ function NavBar() {
             <Link
               to="https://www.linkedin.com/in/will-chesson-484457127/"
               target="_blank"
-              style={{ fontSize: "x-large", color: "white", marginLeft: "10px", marginTop: "5px" }}
+              style={{
+                fontSize: "x-large",
+                color: "white",
+                marginLeft: "10px",
+                marginTop: "5px",
+              }}
             >
               <BsLinkedin />
             </Link>
